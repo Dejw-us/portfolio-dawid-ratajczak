1c97d6807bfc993e9556dea23dccd0902ba081da
Dawid Ratajczak <dawid.ratajczak06+github@gmail.com>
2025-04-08 20:04:20 +0200

    Made page responsive

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import TechStack from "./components/TechStack";
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <div className="mb-32">
-          <a className="text-8xl text-gray-300">Dawid Ratajczak</a>
+      <div className="flex flex-col justify-center md:h-screen items-center px-4">
+        <div className="mb-32 text-center">
+          <a className="text-4xl sm:text-6xl md:text-8xl text-gray-300">
+            Dawid Ratajczak
+          </a>
           <div className="h-1 w-full bg-gradient-to-r from-purple-400 to-pink-600 rounded-xl my-4" />
-          <a className="text-4xl text">portfolio</a>
+          <a className="text-xl sm:text-2xl md:text-4xl">portfolio</a>
         </div>
-        <h3 className="text-6xl">Tech stack</h3>
-        <div className="flex flex-row">
+        <h3 className="text-3xl sm:text-4xl md:text-6xl mb-10">Tech stack</h3>
+        <div className="flex flex-col sm:flex-row gap-8 sm:gap-16 md:gap-32">
           <TechStack lang="Java" framework="Spring boot" />
           <TechStack lang="Typescript" framework="React" />
         </div>
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -5,11 +5,11 @@ export type TechStackProps = {
 
 export default function TechStack({ lang, framework }: TechStackProps) {
   return (
-    <div className="p-[2px] rounded-xl bg-gradient-to-r from-purple-400 to-pink-600 m-20">
-      <div className="flex flex-col bg-black p-20 rounded-xl">
-        <h4 className="text-4xl">{lang}</h4>
-        <a className="text-4xl">+</a>
-        <h4 className="text-4xl">{framework}</h4>
+    <div className="p-[2px] rounded-xl bg-gradient-to-r from-purple-400 to-pink-600 w-full sm:w-auto">
+      <div className="flex flex-col bg-black p-8 sm:p-12 md:p-16 rounded-xl">
+        <h4 className="text-xl sm:text-2xl md:text-4xl">{lang}</h4>
+        <a className="text-xl sm:text-2xl md:text-4xl">+</a>
+        <h4 className="text-xl sm:text-2xl md:text-4xl">{framework}</h4>
       </div>
     </div>
   );
